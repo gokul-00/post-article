06e645c5507433a6506b857cb05012961463be44
gokul-00 <ygokul1702@gmail.com>
2020-06-05 20:10:37 +0530

    adding files

---
+++ models/article.js
@@ -0,0 +1,22 @@
+let mongoose = require('mongoose');
+
+let articleSchema = mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    author:{
+        type: String,
+        required: true
+    },
+    body:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: String,
+        required: true
+    }
+  });
+
+let Article = module.exports = mongoose.model('Article',articleSchema);
---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+
+const UserSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    email:{
+        type:String,
+        required: true
+    },
+    username:{
+        type:String,
+        required: true
+    },
+    password:{
+        type:String,
+        required: true
+    },
+    date:{
+        type:String,
+        required: true
+    }
+    
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
