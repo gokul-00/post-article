7d12e3c4752d6d01ab7e1beda2492b0cff7b00b9
gokul-00 <62957549+gokul-00@users.noreply.github.com>
2020-08-07 20:09:58 +0530

    security debug

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database:'mongodb+srv://gokul:gokul@cluster0.304ke.mongodb.net/nodekb?retryWrites=true&w=majority',
+  database:'mongodb+srv://gokul:<password>@cluster0.304ke.mongodb.net/<database>?retryWrites=true&w=majority',
   secret: 'yoursecret'
 }
